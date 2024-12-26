2037978f21dbb77d5f28b62fb040e3f5869ce104
AjengAlyaK <ajenga990@gmail.com>
2024-12-26 20:54:25 +0700

    fix bug

--- src/Components/ReactButtonGroup.jsx
+++ src/Components/ReactButtonGroup.jsx
@@ -5,18 +5,27 @@ import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 // import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
 // import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { asyncDownVote, asyncNetralVote, asyncUpVote } from '../states/discussion/action';
 
 const ReactButtonGroup = ({ discussionId, userId, likes, dislikes, upVotesBy, downVotesBy, comments, createCommentIcon }) => {
+    const { authUser = null } = useSelector((states) => states)
     const dispatch = useDispatch();
     const upVote = ({ discussionId }) => {
-        dispatch(asyncUpVote({ discussionId }));
+        if (authUser) {
+            dispatch(asyncUpVote({ discussionId }));
+        } else {
+            alert("login first!");
+        }
     };
 
     const downVote = ({ discussionId }) => {
-        dispatch(asyncDownVote({ discussionId }));
-    }
+        if (authUser) {
+            dispatch(asyncDownVote({ discussionId }));
+        } else {
+            alert("login first!");
+        }
+    };
 
     return (
         <Stack direction="row" spacing={2}>
@@ -24,13 +33,13 @@ const ReactButtonGroup = ({ discussionId, userId, likes, dislikes, upVotesBy, do
                 <IconButton aria-label='like' onClick={() => upVote({ discussionId })}>
                     <ThumbUpOutlinedIcon />
                 </IconButton>
-                <Typography sx={{ mr: 1 }}>7</Typography>
+                <Typography sx={{ mr: 1 }}>{likes}</Typography>
             </Box>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <IconButton aria-label='unlike' onClick={() => downVote({ discussionId })}>
                     <ThumbDownOutlinedIcon />
                 </IconButton>
-                <Typography sx={{ mr: 1 }}>7</Typography>
+                <Typography sx={{ mr: 1 }}>{dislikes}</Typography>
             </Box>
             {createCommentIcon && createCommentIcon({ discussionId, comments })}
         </Stack>
