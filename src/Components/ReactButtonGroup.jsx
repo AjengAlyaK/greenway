a1035b316c35c0cd6f6ea15b896a3ef6cdb03d4e
AjengAlyaK <ajenga990@gmail.com>
2024-12-30 14:02:09 +0700

    progress fix bug vote

--- src/Components/ReactButtonGroup.jsx
+++ src/Components/ReactButtonGroup.jsx
@@ -1,4 +1,4 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box, IconButton, Stack, Typography } from '@mui/material';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
@@ -9,11 +9,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import { asyncDownVote, asyncNetralVote, asyncUpVote } from '../states/discussion/action';
 
 const ReactButtonGroup = ({ discussionId, userId, likes, dislikes, upVotesBy, downVotesBy, comments, createCommentIcon }) => {
-    const { authUser = null } = useSelector((states) => states);
     const dispatch = useDispatch();
+    const { authUser = null } = useSelector((states) => states);
+
+    const isLike = upVotesBy.includes(authUser?.id);
+    const isDislike = downVotesBy.includes(authUser?.id);
+
+    const { like, setLike } = useState(isLike);
+    const { dislike, setDislike } = useState(isDislike);
+
     const upVote = ({ discussionId }) => {
         if (authUser) {
-            dispatch(asyncUpVote({ discussionId }));
+            if (isLike) {
+                dispatch(asyncNetralVote({ discussionId }));
+                setLike(!isLike);
+            } else if (!isLike) {
+                dispatch(asyncUpVote({ discussionId }));
+                setLike(!isLike);
+            } else if (isDislike) {
+                dispatch(asyncNetralVote({ discussionId }));
+                dispatch(asyncUpVote({ discussionId }));
+                setDislike(!isDislike);
+            }
         } else {
             alert("login first!");
         }
@@ -30,20 +47,20 @@ const ReactButtonGroup = ({ discussionId, userId, likes, dislikes, upVotesBy, do
     // case click like
     // 1. when button already liked by logged user (DONE)
     // 2. yet liked
-    // 3. when button already unliked bu logged user
+    // 3. when button already unliked by logged user
     // 4. when logout the filled dissapear
 
     // case click dislike
     // 1. when button already disliked by logged user (DONE)
     // 2. yet disliked
-    // 3. when button already liked bu logged user
+    // 3. when button already liked by logged user
     // 4. when logout the filled dissapear
 
     return (
         <Stack direction="row" spacing={2}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <IconButton aria-label='like' onClick={() => upVote({ discussionId })}>
-                    {upVotesBy.includes(authUser?.id) ? <ThumbUpIcon/> : <ThumbUpOutlinedIcon />}
+                    {upVotesBy.includes(authUser?.id) ? <ThumbUpIcon /> : <ThumbUpOutlinedIcon />}
                 </IconButton>
                 <Typography sx={{ mr: 1 }}>{likes}</Typography>
             </Box>
